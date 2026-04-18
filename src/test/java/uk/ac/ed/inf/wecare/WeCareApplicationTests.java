ce2d7275529f5be262f308e5d154882b557a42a7
EllisOsimen <ellisosimen996@gmail.com>
2026-04-18 13:43:01 +0100

    WE care start

---
+++ src/test/java/uk/ac/ed/inf/wecare/WeCareApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ed.inf.wecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
